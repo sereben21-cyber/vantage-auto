a594c450bbf0be4c712b7e7fd956aa325d917bd2
sereben21-cyber <sere.ben21@gmail.com>
2026-02-25 16:57:20 +0100

    Create main homepage component for Vantage Auto car export platform

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+// app/page.tsx
+
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Vantage Auto</h1>
+            <p>Your platform for seamless car exports.</p>
+        </div>
+    );
+};
+
+export default HomePage;
